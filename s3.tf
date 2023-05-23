9b6c67e59b492be9e37352a99af2a82c064bd89c
Ji <cswcsm02@gmail.com>
2023-05-24 00:28:44 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "s3" {
   bucket = "ji-cicd-test"
 
   tags = {
-    Name        = "cicd-test-change"
+    Name        = "cicd-test"
+  }
+}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "ji-cicd-backend"
+
+  tags = {
+    Name        = "cicd-backend"
   }
 }
